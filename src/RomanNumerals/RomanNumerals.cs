1d02a2ce3e77d3a1812d72019c7f95af43e74e33
Antony Denyer <AntonyDenyer@gmail.com>
2011-11-27 21:33:39 +0000

    roman numerals kata

---
+++ src/RomanNumerals/RomanNumerals.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanNumerals
+{
+    public class RomanNumeral
+    {
+        private readonly int _arabicNumber;
+        
+        //it seems simplier to specify out layers rather than calculating the 4,9 and 40 etc
+        private readonly Dictionary<int, string> _numeralMapping = new Dictionary<int, string>
+            {
+                {1, "I"},
+                {4, "IV"},
+                {5, "V"},
+                {9, "IX"},
+                {10, "X"},
+                {40, "XL"},
+                {50, "L"},
+                {90, "XC"},
+                {100, "C"},
+                {400, "CD"},
+                {500, "D"},
+                {900, "CM"},
+                {1000, "M"},
+            };
+
+
+        public RomanNumeral(int arabicNumber)
+        {
+            _arabicNumber = arabicNumber;
+        }
+
+        public new string ToString()
+        {
+            return ToRomanNumeral(_arabicNumber);
+        }
+
+        private string ToRomanNumeral(int arabicNumber)
+        {
+            var romanNumeral = string.Empty;
+            var remainder = arabicNumber;
+            
+            do
+            {
+                var largestNumeral = GetLargestNumeral(remainder);
+                remainder -= largestNumeral.Key;
+                romanNumeral += largestNumeral.Value;
+            
+            } while (remainder > 0);
+
+            return romanNumeral;
+        }
+
+        private KeyValuePair<int,string> GetLargestNumeral(int arabicNumber)
+        {
+            return
+                _numeralMapping.Where(x => x.Key <= arabicNumber).OrderByDescending(x => x.Key).
+                    First();
+        }
+    }
+}
---
+++ src/RomanNumeralsTests/RomanNumeralTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+
+namespace RomanNumerals.Tests
+{
+    public class RomanNumeralTests
+    {
+        [TestFixture]
+        public class when_converting_an_integer_to_a_roman_numeral
+        {
+            [Test]
+            public void should_return_I_for_1()
+            {
+                var result = new RomanNumeral(1);
+                Assert.That(result.ToString(), Is.EqualTo("I"));
+            }
+
+            [Test]
+            public void should_return_II_for_2()
+            {
+                var result = new RomanNumeral(2);
+                Assert.That(result.ToString(), Is.EqualTo("II"));
+            }
+
+            [Test]
+            public void should_return_V_for_5()
+            {
+                var result = new RomanNumeral(5);
+                Assert.That(result.ToString(), Is.EqualTo("V"));
+            }
+
+            [Test]
+            public void should_return_VI_for_6()
+            {
+                var result = new RomanNumeral(6);
+                Assert.That(result.ToString(), Is.EqualTo("VI"));
+            }
+
+            [Test]
+            public void should_return_IV_for_4()
+            {
+                var result = new RomanNumeral(4);
+                Assert.That(result.ToString(), Is.EqualTo("IV"));
+            }
+
+            [Test]
+            public void should_return_IX_for_9()
+            {
+                var result = new RomanNumeral(9);
+                Assert.That(result.ToString(), Is.EqualTo("IX"));
+            }
+
+            [Test]
+            public void should_return_XLIX_for_49()
+            {
+                var result = new RomanNumeral(49);
+                Assert.That(result.ToString(), Is.EqualTo("XLIX"));
+            }
+
+            [Test]
+            public void should_return_XCIX_for_99()
+            {
+                var result = new RomanNumeral(99);
+                Assert.That(result.ToString(), Is.EqualTo("XCIX"));
+            }
+
+            [Test]
+            public void should_return_bbc_format_for_1999()
+            {
+                var result = new RomanNumeral(1999);
+                Assert.That(result.ToString(), Is.EqualTo("MCMXCIX"));
+            }
+        }
+    }
+}
