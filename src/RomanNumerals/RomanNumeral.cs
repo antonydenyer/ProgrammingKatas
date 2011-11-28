19e7097b6d43b2af8066de9bd457d56a2c202602
Antony Denyer <AntonyDenyer@gmail.com>
2011-11-28 23:13:47 +0000

    parse the numerals and put an int out

--- src/RomanNumerals/RomanNumeral.cs
+++ src/RomanNumerals/RomanNumeral.cs
@@ -5,8 +5,9 @@ namespace RomanNumerals
 {
     public class RomanNumeral
     {
+        private readonly string _romanNumeral;
         private readonly int _arabicNumber;
-        
+
         //it seems simplier to specify out layers rather than calculating the 4,9 and 40 etc
         private readonly List<RomanNumeralMap> _numeralMapping = new List<RomanNumeralMap>
             {
@@ -31,22 +32,75 @@ namespace RomanNumerals
             _arabicNumber = arabicNumber;
         }
 
-        public new string ToString()
+        public RomanNumeral(string romanNumeral)
+        {
+            _romanNumeral = romanNumeral;
+        }
+
+
+        public string ToNumeral()
         {
             return ToRomanNumeral(_arabicNumber);
+        }
+
+        public int ToInt()
+        {
+            return ParseRomanNumeral(_romanNumeral);
+        }
+
+        private int ParseRomanNumeral(string romanNumeral)
+        {
+            var arabicNumber = 0;
+
+            var numeralArray = romanNumeral.ToCharArray();
+
+            for (var i = numeralArray.Length - 1; i >= 0; i--)
+            {
+                if (i > 0 && ContainsNumeral(DuoNumeral(numeralArray[i - 1], numeralArray[i])))
+                {
+                    arabicNumber += GetintegerForNumeral(DuoNumeral(numeralArray[i - 1], numeralArray[i]));
+                    i--;
+                }
+                else
+                {
+                    arabicNumber += GetintegerForNumeral(numeralArray[i].ToString());
+                }
+            }
+
+            return arabicNumber;
+        }
+
+        public bool ContainsNumeral(string numeral)
+        {
+            return GetNumeral(numeral) != null;
+        }
+
+        private int GetintegerForNumeral(string numeral)
+        {
+            return GetNumeral(numeral).ArabicNumber;
+        }
+
+        private static string DuoNumeral(char one, char two)
+        {
+            return string.Format("{0}{1}", one, two);
+        }
+
+        private RomanNumeralMap GetNumeral(string numeral)
+        {
+            return _numeralMapping.Where(x => x.RomanNumeral == numeral).FirstOrDefault();
         }
 
         private string ToRomanNumeral(int arabicNumber)
         {
             var romanNumeral = string.Empty;
             var remainder = arabicNumber;
-            
+
             do
             {
                 var largestNumeral = GetLargestNumeral(remainder);
                 remainder -= largestNumeral.ArabicNumber;
                 romanNumeral += largestNumeral.RomanNumeral;
-            
+
             } while (remainder > 0);
 
             return romanNumeral;
@@ -58,5 +112,6 @@ namespace RomanNumerals
                 _numeralMapping.Where(x => x.ArabicNumber <= arabicNumber).OrderByDescending(x => x.ArabicNumber).
                     First();
         }
+
     }
 }
--- src/RomanNumeralsTests/RomanNumeralTests.cs
+++ src/RomanNumeralsTests/RomanNumeralTests.cs
@@ -11,63 +11,116 @@ namespace RomanNumerals.Tests
             public void should_return_I_for_1()
             {
                 var result = new RomanNumeral(1);
-                Assert.That(result.ToString(), Is.EqualTo("I"));
+                Assert.That(result.ToNumeral(), Is.EqualTo("I"));
             }
 
             [Test]
             public void should_return_II_for_2()
             {
                 var result = new RomanNumeral(2);
-                Assert.That(result.ToString(), Is.EqualTo("II"));
+                Assert.That(result.ToNumeral(), Is.EqualTo("II"));
             }
 
             [Test]
             public void should_return_V_for_5()
             {
                 var result = new RomanNumeral(5);
-                Assert.That(result.ToString(), Is.EqualTo("V"));
+                Assert.That(result.ToNumeral(), Is.EqualTo("V"));
             }
 
             [Test]
             public void should_return_VI_for_6()
             {
                 var result = new RomanNumeral(6);
-                Assert.That(result.ToString(), Is.EqualTo("VI"));
+                Assert.That(result.ToNumeral(), Is.EqualTo("VI"));
             }
 
             [Test]
             public void should_return_IV_for_4()
             {
                 var result = new RomanNumeral(4);
-                Assert.That(result.ToString(), Is.EqualTo("IV"));
+                Assert.That(result.ToNumeral(), Is.EqualTo("IV"));
             }
 
             [Test]
             public void should_return_IX_for_9()
             {
                 var result = new RomanNumeral(9);
-                Assert.That(result.ToString(), Is.EqualTo("IX"));
+                Assert.That(result.ToNumeral(), Is.EqualTo("IX"));
             }
 
             [Test]
             public void should_return_XLIX_for_49()
             {
                 var result = new RomanNumeral(49);
-                Assert.That(result.ToString(), Is.EqualTo("XLIX"));
+                Assert.That(result.ToNumeral(), Is.EqualTo("XLIX"));
             }
 
             [Test]
             public void should_return_XCIX_for_99()
             {
                 var result = new RomanNumeral(99);
-                Assert.That(result.ToString(), Is.EqualTo("XCIX"));
+                Assert.That(result.ToNumeral(), Is.EqualTo("XCIX"));
             }
 
             [Test]
             public void should_return_bbc_format_for_1999()
             {
                 var result = new RomanNumeral(1999);
-                Assert.That(result.ToString(), Is.EqualTo("MCMXCIX"));
+                Assert.That(result.ToNumeral(), Is.EqualTo("MCMXCIX"));
+            }
+        }
+
+        [TestFixture]
+        public class when_converting_an_roman_numeral_to_an_integer
+        {
+            [Test]
+            public void should_return_1_for_I()
+            {
+                var result = new RomanNumeral("I");
+                Assert.That(result.ToInt(), Is.EqualTo(1));
+            }
+
+            [Test]
+            public void should_return_2_for_II()
+            {
+                var result = new RomanNumeral("II");
+                Assert.That(result.ToInt(), Is.EqualTo(2));
+            }
+
+            [Test]
+            public void should_return_4_for_IV()
+            {
+                var result = new RomanNumeral("IV");
+                Assert.That(result.ToInt(), Is.EqualTo(4));
+            }
+
+            [Test]
+            public void should_return_9_forIX()
+            {
+                var result = new RomanNumeral("IX");
+                Assert.That(result.ToInt(), Is.EqualTo(9));
+            }
+
+            [Test]
+            public void should_return_XLIX_for_49()
+            {
+                var result = new RomanNumeral("XLIX");
+                Assert.That(result.ToInt(), Is.EqualTo(49));
+            }
+
+            [Test]
+            public void should_return_99_for_XCIX()
+            {
+                var result = new RomanNumeral("XCIX");
+                Assert.That(result.ToInt(), Is.EqualTo(99));
+            }
+
+            [Test]
+            public void should_return_1999_for_bbc()
+            {
+                var result = new RomanNumeral("MCMXCIX");
+                Assert.That(result.ToInt(), Is.EqualTo(1999));
             }
         }
     }
