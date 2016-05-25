aff4d823dec7b5652c147a8a46b251355ffe638f
Antony Denyer <antonydenyer@gmail.com>
2016-05-25 11:37:02 +0100

    Green: should apply discount for three basket entry A

--- ruby/lib/checkout.rb
+++ ruby/lib/checkout.rb
@@ -3,14 +3,20 @@ class Checkout
   def initialize(unit_prices)
     @unit_prices = unit_prices
     @total = 0
+    @items = []
   end
 
   def scan(sku)
+    @items << sku
     @total += @unit_prices[sku]
   end
 
   def total
-    @total
+    discount = 0
+    if @items.count{|item| item == 'A'} == 3
+      discount = 20
+    end
+    @total - discount
   end
 
 end
