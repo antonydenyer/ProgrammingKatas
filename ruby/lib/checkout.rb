fa0941fd2e0cdf9ba3859ccb62a937622135634f
Antony Denyer <antonydenyer@gmail.com>
2016-05-25 11:55:15 +0100

    Refactor: should apply discount for two basket entry B

--- ruby/lib/checkout.rb
+++ ruby/lib/checkout.rb
@@ -3,24 +3,17 @@ class Checkout
   def initialize(unit_prices)
     @unit_prices = unit_prices
     @total = 0
-    @items = []
+    @sku_count = Hash.new(0)
   end
 
   def scan(sku)
-    @items << sku
+    @sku_count[sku] += 1
     @total += @unit_prices[sku]
   end
 
   def total
     discount = 0
-
-    sku_count = @items.inject(Hash.new(0)) do |hash, sku|
-      hash[sku] += 1
-      hash
-    end
-
-
-    sku_count.each do | sku, count |
+    @sku_count.each do | sku, count |
       if sku == 'A'
         discount += count / 3 * 20
       end
