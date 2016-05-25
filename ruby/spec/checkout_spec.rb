e3ce6a4ac9fd6f68cdc1913a02e9a6cde70db734
Antony Denyer <antonydenyer@gmail.com>
2016-05-25 11:21:09 +0100

    Red: should calculate total for single basket entry A

--- ruby/spec/checkout_spec.rb
+++ ruby/spec/checkout_spec.rb
@@ -1,10 +1,15 @@
 require 'checkout'
 
 describe Checkout do
-  let(:checkout){ Checkout.new }
+  let(:checkout){ Checkout.new() }
 
   it 'total is zero when basket is empty' do
     expect(checkout.total).to be 0
+  end
+
+  it 'should calculate total for single basket entry A' do
+    checkout.scan('A')
+    expect(checkout.total).to be 50
   end
 
 end
